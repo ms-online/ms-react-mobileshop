a5b2c33d87a3c62dad0f11a15d94d0b19b689f6f
ms-online <27732357@qq.com>
2020-11-28 12:50:45 +0800

    lesson-19

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import Product from '../models/productModel.js'
+const router = express.Router()
+
+import asyncHandler from 'express-async-handler'
+
+// @desc    请求所有产品
+// @route   GET /api/products
+// @access  公开的
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+    res.json(products)
+  })
+)
+
+// @desc    请求单个产品
+// @route   GET /api/products/:id
+// @access  公开的
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    if (product) {
+      res.json(product)
+    } else {
+      res.status(404).json({ message: '查找不到该产品！' })
+    }
+  })
+)
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import products from './data/products.js'
 import connectDB from './config/db.js'
 import dotenv from 'dotenv'
 import colors from 'colors'
+
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 connectDB()
@@ -11,13 +12,8 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('API正在运行......')
 })
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+
+app.use('/api/products', productRoutes)
 
 const PORT = process.env.PORT || 5000
 
