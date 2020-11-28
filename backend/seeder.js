9e1cec19dba4f078b65447301da0105d47af0eba
ms-online <27732357@qq.com>
2020-11-28 11:48:52 +0800

    lesson-18

--- backend/seeder.js
+++ backend/seeder.js
@@ -12,21 +12,24 @@ dotenv.config()
 
 connectDB()
 
-//插入数据
+//插入数据到数据库
 const importData = async () => {
   try {
+    //初始化清空内容
     await Order.deleteMany()
     await Product.deleteMany()
     await User.deleteMany()
 
-    const createdUsers = await User.insertMany(users)
-    const adminUser = createdUsers[0]._id
+    //插入数据
+    const createdUser = await User.insertMany(users)
+    const adminUser = createdUser[0]._id
+
     const sampleProducts = products.map((product) => {
       return { ...product, user: adminUser }
     })
 
     await Product.insertMany(sampleProducts)
-    console.log('样本数据已经插入数据库'.green.inverse)
+    console.log('样本数据已经实现存储！'.green.inverse)
     process.exit()
   } catch (error) {
     console.log(`${error}`.red.inverse)
@@ -34,21 +37,22 @@ const importData = async () => {
   }
 }
 
-//销毁数据
+//销毁数据库中的数据
 const destroyData = async () => {
   try {
+    //初始化清空内容
     await Order.deleteMany()
     await Product.deleteMany()
     await User.deleteMany()
-    console.log('样本数据已被销毁'.red.inverse)
+
+    console.log('样本数据已被销毁！'.green.inverse)
     process.exit()
   } catch (error) {
     console.log(`${error}`.red.inverse)
     process.exit(1)
   }
 }
-
-// 获取命令行执行的node程序，调用对应的函数
+//判断是销毁还是保存数据
 if (process.argv[2] === '-d') {
   destroyData()
 } else {
